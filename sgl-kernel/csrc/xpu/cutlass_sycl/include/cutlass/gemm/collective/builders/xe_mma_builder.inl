f35c631be10427a7b7aaa5462042da079a7404ad
Sun, Jiwei1 <jiwei1.sun@intel.com>
2025-06-04 02:41:35 +0800

    add include for cutlass

---
+++ sgl-kernel/csrc/xpu/cutlass_sycl/include/cutlass/gemm/collective/builders/xe_mma_builder.inl
@@ -0,0 +1,228 @@
+/***************************************************************************************************
+ * Copyright (c) 2024 - 2024 Codeplay Software Ltd. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **************************************************************************************************/
+
+#pragma once
+
+#include <cutlass/arch/arch.h>
+#include <cutlass/gemm/dispatch_policy.hpp>
+
+#include "cutlass/gemm/collective/collective_mma.hpp"
+
+namespace cutlass::gemm::collective {
+
+  // Intel Xe 3 stage pipeline, using prefetch
+  // Also the auto builder
+
+template<typename MMAAtom, typename T_m, typename T_n>
+constexpr auto get_num_atoms(T_m tile_m, T_n tile_n){
+  constexpr auto atom_m = get<0>(typename MMAAtom::Shape_MNK{});
+  constexpr auto atom_n = get<1>(typename MMAAtom::Shape_MNK{});
+  // try to create the biggest number of atoms possible, up to 32, trying to fit the most, up to 8 in m dimension
+  auto atoms_m_tmp = cute::min(tile_m / atom_m, _8{}); // at most 8
+  auto atoms_n = cute::min(tile_n / atom_n, _32{} / atoms_m_tmp); // at most however many are not in m out of 32
+  auto atoms_m = cute::min(tile_m / atom_m, _32{} / atoms_n);  // at most however many are not in n out of 32
+  return make_shape(atoms_m, atoms_n);
+}
+
+template<bool is_t, bool is_v, typename T_m, typename T_n>
+constexpr auto select_copy_atom_16b(T_m tile_m, T_n tile_n){
+  #define RETURN_ATOM(WIDTH, HEIGHT, LETTER) \
+    return XE_2D_U16x##WIDTH##x##HEIGHT##_LD_##LETTER {};
+
+  if constexpr(is_t){
+    // tile_m and tile_n have swapped role in case of _T
+    static_assert(tile_n % 16 == 0 && "Invalid tile_m");
+    if constexpr(tile_m == 8){
+      RETURN_ATOM(16, 8, T)
+    } else if constexpr(tile_m % 16 == 0){
+      RETURN_ATOM(16, 16, T)
+    } else{
+      static_assert(dependent_false<T_m> && "Invalid tile_n");
+    }
+  } else if constexpr(is_v){
+    #define SELECT_HEIGHT_V(WIDTH) \
+      if constexpr(tile_n == 16){ \
+        RETURN_ATOM(WIDTH, 16, V) \
+      } else if constexpr(tile_n % 32 == 0){ \
+        RETURN_ATOM(WIDTH, 32, V) \
+      } else{ \
+        static_assert(dependent_false<T_n> && "Invalid tile_n"); \
+      }
+
+    if constexpr(tile_m == 16){
+      SELECT_HEIGHT_V(16)
+    } else if constexpr(tile_m % 32 == 0){
+      SELECT_HEIGHT_V(32)
+    } else{
+      static_assert(dependent_false<T_m> && "Invalid tile_m");
+    }
+    #undef SELECT_HEIGHT_V
+  } else{ // _N
+    #define SELECT_WIDTH_N(HEIGHT) \
+      if constexpr(tile_m == 1){ \
+        RETURN_ATOM(1, HEIGHT, N) \
+      } else if constexpr(tile_m == 2){ \
+        RETURN_ATOM(2, HEIGHT, N) \
+      } else if constexpr(tile_m == 4){ \
+        RETURN_ATOM(4, HEIGHT, N) \
+      } else if constexpr(tile_m == 8){ \
+        RETURN_ATOM(8, HEIGHT, N) \
+      } else if constexpr(tile_m == 16){ \
+        RETURN_ATOM(16, HEIGHT, N) \
+      } else if constexpr(tile_m % 32 == 0){ \
+        RETURN_ATOM(32, HEIGHT, N) \
+      } else { \
+        static_assert(dependent_false<T_m> && "Invalid tile_m"); \
+      }
+
+    if constexpr(tile_n == 16){
+      SELECT_WIDTH_N(16)
+    } else if constexpr(tile_n % 32 == 0){
+      SELECT_WIDTH_N(32)
+    } else {
+      static_assert(dependent_false<T_n> && "Invalid tile_n");
+    }
+    #undef SELECT_WIDTH_N
+  }
+  #undef RETURN_ATOM
+}
+
+namespace {
+template <typename ElementAB, typename ElementCD>
+struct pick_mma_atom{
+  static_assert(dependent_false<ElementAB> && "no mma atom for this combination of types");
+};
+
+#define PICK_MMA(ElementAB, ElementCD, ATOM)             \
+template <> struct pick_mma_atom<ElementAB, ElementCD> { \
+  using atom = MMA_Atom<ATOM>;                           \
+};
+
+PICK_MMA(bfloat16_t, float, XE_8x16x16_F32BF16BF16F32_TT);
+PICK_MMA(bfloat16_t, bfloat16_t, XE_8x16x16_BF16BF16BF16BF16_TT);
+PICK_MMA(half_t, float, XE_8x16x16_F32F16F16F32_TT);
+
+#undef PICK_MMA
+}
+
+template <
+  class ElementA,
+  class GmemLayoutATag,
+  int AlignmentA,
+  class ElementB,
+  class GmemLayoutBTag,
+  int AlignmentB,
+  class ElementAccumulator,
+  class TileShape_MNK,
+  class KernelScheduleType
+  >
+struct CollectiveBuilder<
+  arch::IntelXe,
+  arch::OpClassTensorOp,   // Reusing opClassTensorOp for Intel devices
+  ElementA,
+  GmemLayoutATag,
+  AlignmentA,
+  ElementB,
+  GmemLayoutBTag,
+  AlignmentB,
+  ElementAccumulator,
+  TileShape_MNK,
+  Shape<_1, _1, _1>,    // Cluster Shape
+  cutlass::gemm::collective::StageCountAuto,
+  KernelScheduleType,
+  cute::enable_if_t<
+    cute::is_any_of_v<KernelScheduleType, KernelScheduleAuto, KernelXe, KernelXeCooperative, KernelXePtrArrayCooperative> &&
+    cute::is_same_v<ElementA, ElementB> &&
+    cute::is_any_of_v<ElementA, bfloat16_t, half_t> &&
+    cute::is_any_of_v<ElementB, bfloat16_t, half_t>
+  >
+    >{
+
+      #ifdef SYCL_NVIDIA_TARGET
+        static_assert(cutlass::detail::dependent_false<arch::IntelXe>,
+          "Trying to use Intel pipeline on Non Intel hardware");
+      #endif
+      static_assert(is_static<TileShape_MNK>::value);
+      static_assert(cute::is_any_of_v<ElementAccumulator, float, bfloat16_t>, "Intel multi-stage pipeline requires ElementC to be of type float or bfloat");
+
+      using MMAAtom = typename pick_mma_atom<ElementA, ElementAccumulator>::atom;
+
+      static constexpr auto tile_M = get<0>(TileShape_MNK{});
+      static constexpr auto tile_N = get<1>(TileShape_MNK{});
+      static constexpr auto tile_K = get<2>(TileShape_MNK{});
+
+      static constexpr auto n_atoms = get_num_atoms<MMAAtom>(tile_M, tile_N);
+      using atoms_M = decltype(get<0>(n_atoms));
+      using atoms_N = decltype(get<1>(n_atoms));
+      using TiledMma =
+          typename TiledMMAHelper<MMAAtom,
+                                  Layout<TileShape_MNK>,
+                                  Layout<Shape<atoms_M, atoms_N, _1>, Stride<atoms_N, _1, _0>>>::TiledMMA;
+
+      static constexpr bool IsGroup = cute::is_same_v<KernelScheduleType, KernelXePtrArrayCooperative>;
+
+      using KernelSchedule = std::conditional_t<cute::is_same_v<KernelScheduleType, KernelScheduleAuto>, KernelXe, KernelScheduleType>;
+      static constexpr int PipelineStages = IsGroup ? 2 : 3;
+      using DispatchPolicy = std::conditional_t<IsGroup,
+                                                cutlass::gemm::MainloopIntelXeXMX16Group<PipelineStages, KernelSchedule>,
+                                                cutlass::gemm::MainloopIntelXeXMX16<PipelineStages, KernelSchedule>>;
+
+      using GmemTiledCopyA = decltype(select_copy_atom_16b<cute::is_same_v<GmemLayoutATag, cutlass::layout::ColumnMajor>, false>(tile_M/atoms_M{}, tile_K));
+      using GmemTiledCopyB = decltype(select_copy_atom_16b<cute::is_same_v<GmemLayoutBTag, cutlass::layout::ColumnMajor>, true>(tile_K, tile_N/atoms_N{}));
+
+      // Xe pipeline does not use shared memory
+      using SmemLayoutAtomA = void;
+      using SmemLayoutAtomB = void;
+      using SmemCopyAtomA = void;
+      using SmemCopyAtomB = void;
+
+      using TransformA = cute::identity;
+      using TransformB = cute::identity;
+
+      using CollectiveOp = cutlass::gemm::collective::CollectiveMma<
+              DispatchPolicy,
+              TileShape_MNK,
+              ElementA,
+              cutlass::gemm::TagToStrideA_t<std::conditional_t<IsGroup, GmemLayoutATag*, GmemLayoutATag>>,
+              ElementB,
+              cutlass::gemm::TagToStrideB_t<std::conditional_t<IsGroup, GmemLayoutBTag*, GmemLayoutBTag>>,
+              TiledMma,
+              GmemTiledCopyA,
+              SmemLayoutAtomA,
+              SmemCopyAtomA,
+              TransformA,
+              GmemTiledCopyB,
+              SmemLayoutAtomB,
+              SmemCopyAtomB,
+              TransformB
+          >;
+    };
+}
---
+++ sgl-kernel/csrc/xpu/cutlass_sycl/include/cutlass/gemm/collective/xe_mma_w8a8.hpp
@@ -0,0 +1,299 @@
+/***************************************************************************************************
+ * Copyright (c) 2025 - 2025 Codeplay Software Ltd. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ **************************************************************************************************/
+#pragma once
+
+#include "cutlass/cutlass.h"
+#include "cutlass/gemm/dispatch_policy.hpp"
+#include "cutlass/fp8_to_fp16.h"
+
+#include "cute/algorithm/functional.hpp"
+#include "cute/atom/mma_atom.hpp"
+#include "cute/algorithm/gemm.hpp"
+#include "cute/tensor_predicate.hpp"
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+namespace cutlass::gemm::collective {
+using namespace cute;
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <int Stages, class Schedule, class TileShape_, class ElementA_, class StrideA_, class ElementB_, class StrideB_,
+          class TiledMma_, class GmemTiledCopyA_, class SmemLayoutAtomA_, class SmemCopyAtomA_, class TransformA_,
+          class GmemTiledCopyB_, class SmemLayoutAtomB_, class SmemCopyAtomB_, class TransformB_>
+struct CollectiveMma<MainloopIntelW8A8<Stages, Schedule>, TileShape_, ElementA_, StrideA_, ElementB_, StrideB_, TiledMma_,
+                     GmemTiledCopyA_, SmemLayoutAtomA_, SmemCopyAtomA_, TransformA_, GmemTiledCopyB_, SmemLayoutAtomB_,
+                     SmemCopyAtomB_, TransformB_> {
+  //
+  // Type Aliases
+  //
+  using DispatchPolicy = MainloopIntelW8A8<Stages, Schedule>;
+  using WorkgroupTileShape = TileShape_;
+  using ElementA = ElementA_;
+  using StrideA = StrideA_;
+  using ElementB = ElementB_;
+  using StrideB = StrideB_;
+  using TiledMma = TiledMma_;
+  using ElementAccumulator = typename TiledMma::ValTypeC;
+  using GmemTiledCopyA = GmemTiledCopyA_;
+  using GmemTiledCopyB = GmemTiledCopyB_;
+  using SmemLayoutAtomA = SmemLayoutAtomA_;
+  using SmemLayoutAtomB = SmemLayoutAtomB_;
+  using SmemCopyAtomA = SmemCopyAtomA_;
+  using SmemCopyAtomB = SmemCopyAtomB_;
+  using TransformA = TransformA_;
+  using TransformB = TransformB_;
+  using ArchTag = typename DispatchPolicy::ArchTag;
+
+  static_assert(platform::is_same<ElementA, ElementB>::value, "MainloopIntelW8A8 requires that A and B have same type.");
+  static_assert(std::is_same_v<TransformA, cute::identity>, "Transformation for A is not currently supported on Intel PVC");
+  static_assert(std::is_same_v<TransformB, cute::identity>, "Transformation for B is not currently supported on Intel PVC");
+
+  static constexpr int SubgroupSize = DispatchPolicy::SubgroupSize;
+
+  using MmaAtomShape = typename TiledMma::AtomShape_MNK;
+
+  static constexpr auto BLK_M = get<0>(WorkgroupTileShape{});
+  static constexpr auto BLK_N = get<1>(WorkgroupTileShape{});
+  static constexpr auto BLK_K = get<2>(WorkgroupTileShape{});
+
+  static constexpr auto ATOM_M = get<1>(typename TiledMma::ThrLayoutVMNK{}.shape());
+  static constexpr auto ATOM_N = get<2>(typename TiledMma::ThrLayoutVMNK{}.shape());
+  static constexpr auto ATOM_K = get<3>(typename TiledMma::ThrLayoutVMNK{}.shape());
+
+  static_assert(BLK_M % TiledMma{}.template tile_size_mnk<0>() == 0, "TiledMma permutation size must match block size.");
+  static_assert(BLK_N % TiledMma{}.template tile_size_mnk<1>() == 0, "TiledMma permutation size must match block size.");
+  static_assert(BLK_K % TiledMma{}.template tile_size_mnk<2>() == 0, "TiledMma permutation size must match block size.");
+
+  static constexpr auto SG_M = ceil_div(BLK_M, ATOM_M);
+  static constexpr auto SG_N = ceil_div(BLK_N, ATOM_N);
+  static constexpr auto SG_K = ceil_div(BLK_K, ATOM_K);
+  using SubgroupTileShape = Shape<decltype(SG_M), decltype(SG_N), decltype(SG_K)>;
+
+  // 32
+  static constexpr auto Num_SGs = ATOM_N * ATOM_M * ATOM_K;
+  static constexpr uint32_t MaxThreadsPerBlock = size(TiledMma{});
+
+  using CopyThreadShape = Shape<_1, Int<SubgroupSize>>;
+
+  using traits_load_A = Copy_Traits<GmemTiledCopyA, StrideA>;
+  using atom_load_A = Copy_Atom<traits_load_A, ElementA>;
+  using val_layout_load_A = decltype(make_layout(shape_div(typename traits_load_A::BlockShape{}, CopyThreadShape{})));
+  using Copy_A = decltype(make_tiled_copy(atom_load_A{}, Layout<CopyThreadShape>{}, val_layout_load_A{}));
+
+  using traits_load_B = Copy_Traits<GmemTiledCopyB, StrideB>;
+  using atom_load_B = Copy_Atom<traits_load_B, ElementB>;
+  using val_layout_load_B = decltype(make_layout(shape_div(typename traits_load_B::BlockShape{}, CopyThreadShape{})));
+  using Copy_B = decltype(make_tiled_copy(atom_load_B{}, Layout<CopyThreadShape>{}, val_layout_load_B{}));
+
+  // Host side kernel arguments
+  struct Arguments {
+    ElementA const* ptr_A;
+    StrideA dA;
+    ElementB const* ptr_B;
+    StrideB dB;
+  };
+
+  struct Params {
+    Copy_A tiled_copy_a;
+    Copy_B tiled_copy_b;
+  };
+
+  //
+  // Methods
+  //
+
+  CollectiveMma() = default;
+
+  template <class ProblemShape>
+  static constexpr Params
+  to_underlying_arguments(ProblemShape const& problem_shape, Arguments const& args, void* workspace) {
+    (void) workspace;
+
+    auto [M,N,K,L] = problem_shape;
+
+    auto mA_mkl = make_tensor(make_gmem_ptr(args.ptr_A), make_layout(make_shape(M, K, L), args.dA));
+    auto mB_nkl = make_tensor(make_gmem_ptr(args.ptr_B), make_layout(make_shape(N, K, L), args.dB));
+    Copy_A tiled_copy_a{Copy_A{}.with(mA_mkl)};
+    Copy_B tiled_copy_b{Copy_B{}.with(mB_nkl)};
+    
+    return Params{tiled_copy_a, tiled_copy_b};
+  }
+
+  template <class EngineIn,
+      class EngineOut,
+      class LayoutIn,
+      class LayoutOut,
+      class... Ts>
+  CUTLASS_DEVICE
+  void convert_FP8_to_FP16(
+          Tensor<EngineIn, LayoutIn> const& in,
+          Tensor<EngineOut, LayoutOut>& out) {
+
+    static_assert(is_rmem<EngineIn>::value, "Input tensor for A conversion must come from registers");
+    static_assert(is_rmem<EngineOut>::value, "Output tensor for A conversion must come from registers");
+    static_assert(cosize_v<LayoutIn> == cosize_v<LayoutOut>);
+    static_assert(size_v<LayoutIn> == cosize_v<LayoutIn>);
+    static_assert(size_v<LayoutOut> == cosize_v<LayoutOut>);
+
+    using SrcType = typename EngineIn::value_type;
+    using DstType = typename EngineOut::value_type;
+
+    static_assert(std::is_same_v<SrcType, uint8_t>, "Expected fp8 (E4M3) input as uint8_t");
+    static_assert(std::is_same_v<DstType, half_t>, "Expected fp16 output as half_t");
+
+    auto const& src = in(_, _, _);
+    auto const& dst = out(_, _, _);
+
+    SrcType const* pSrc = src.data();
+    DstType* pDst = dst.data();
+
+    constexpr int num_elements = decltype(size(src))::value;
+    // TODO(Codeplay): Move conversion to NumericArrayConverter
+    if constexpr (std::is_same_v<ElementA, float_e5m2_t>) {
+      // Using something as simple as the following code surprisingly
+      // leads to poor performance.
+      // CUTLASS_PRAGMA_UNROLL
+      // for (int i = 0; i < num_elements; i++) {
+      //   reinterpret_cast<uint16_t*>(pDst)[i] = (static_cast<uint16_t>((pSrc[i]))) << 8;
+      // }
+      // The root-cause is unknown, but private memory use is seen in this case.
+      using SrcArray = cutlass::Array<uint8_t, num_elements>;
+      using DstArray = cutlass::Array<uint16_t, num_elements>;
+      SrcArray const* pSrcArr = reinterpret_cast<SrcArray const*>(pSrc);
+      DstArray* pDstArr = reinterpret_cast<DstArray*>(pDst);
+      E5M2_to_FP16<num_elements>(*pSrcArr, *pDstArr);
+    } else {
+      // E4M3 -> FP16 conversion
+      constexpr int chunk_size = 16;
+      constexpr int iters = num_elements / chunk_size;
+      CUTLASS_PRAGMA_UNROLL
+      for (int i = 0; i < iters; ++i) {
+        cute::intel::uchar16 src_vec;
+        CUTLASS_PRAGMA_UNROLL
+        for (int j = 0; j < chunk_size; ++j) {
+          src_vec[j] = pSrc[i * chunk_size + j];
+        }
+        cute::intel::ushort16 dst_vec;
+        dst_vec = E4M3_to_FP16_chunk16(src_vec);
+        CUTLASS_PRAGMA_UNROLL
+        for (int j = 0; j < chunk_size; ++j) {
+          reinterpret_cast<uint16_t*>(pDst)[i * chunk_size + j] = dst_vec[j];
+        }
+      }
+    }
+  }
+
+  /// Perform a subgroup-scoped matrix multiply-accumulate
+  template <class FrgTensorD, class TensorA, class TensorB, class FrgTensorC, class KTileIterator, class BlkCoord>
+  CUTLASS_DEVICE void operator()(FrgTensorD &accum, TensorA gA, TensorB gB, FrgTensorC const &src_accum,
+                                 KTileIterator k_tile_iter, int k_tile_count, BlkCoord const &blk_coord, int const &K_start, int thread_idx,
+                                 Params const &mainloop) {
+    (void)blk_coord;
+    static_assert(is_rmem<FrgTensorD>::value, "D tensor must be rmem resident.");
+    static_assert(is_rmem<FrgTensorC>::value, "C tensor must be rmem resident.");
+
+    auto thr_copy_A = mainloop.tiled_copy_a.get_slice(thread_idx);
+    auto thr_copy_B = mainloop.tiled_copy_b.get_slice(thread_idx);
+
+    // Instantiate the MMA object and get thread slice
+    TiledMma tiled_mma;
+    // TODO(Codeplay): see if we can make this nicer
+    // To make all work items in a subgroup have the same global tensors pass in the index of work item 0 in each subgroup
+    auto sg = syclcompat::get_nd_item<1>().get_sub_group();
+    auto first_thread_in_sg_idx = sg.get_group_linear_id() * DispatchPolicy::SubgroupSize;
+    auto thr_mma = tiled_mma.get_slice(first_thread_in_sg_idx);
+
+    // Partition global counting tensors for MMA
+    Tensor tCgA = thr_mma.partition_A(gA);
+    Tensor tCgB = thr_mma.partition_B(gB);
+
+    Tensor tCrA = make_tensor<uint8_t>(make_fragment_layout(mainloop.tiled_copy_a, tCgA(_,_,_,0).shape()));
+    Tensor tCrB = make_tensor<uint8_t>(make_fragment_layout(mainloop.tiled_copy_b, tCgB(_,_,_,0).shape()));
+
+    Tensor tCrA_fp16 = make_fragment_like<half_t>(tCrA);
+    Tensor tCrB_fp16 = make_fragment_like<half_t>(tCrB);
+
+    // Retile registers for copies
+    Tensor tArA = thr_copy_A.retile_D(tCrA);
+    Tensor tBrB = thr_copy_B.retile_D(tCrB);
+    
+    // Retile global counting tensors for copies
+    Tensor tAgA = thr_copy_A.retile_S(tCgA);
+    Tensor tBgB = thr_copy_B.retile_S(tCgB);
+    
+    auto tiled_prefetch_a = cute::prefetch_selector<Shape<Int<BLK_M>,Int<BLK_K>>, Num_SGs>(mainloop.tiled_copy_a);
+    auto tiled_prefetch_b = cute::prefetch_selector<Shape<Int<BLK_N>,Int<BLK_K>>, Num_SGs>(mainloop.tiled_copy_b);
+    auto thr_prefetch_A = tiled_prefetch_a.get_slice(thread_idx);
+    auto thr_prefetch_B = tiled_prefetch_b.get_slice(thread_idx);
+    
+    // Partition global tile for prefetch
+    auto pAgA = thr_prefetch_A.partition_S(gA);
+    auto pBgB = thr_prefetch_B.partition_S(gB);
+
+    //
+    // Mainloop
+    //
+    const auto k_start_idx = crd2idx((*k_tile_iter), make_shape(K_start));
+    constexpr int barrier_scope = 2;
+    int prefetch_k = k_start_idx;
+
+    CUTLASS_PRAGMA_UNROLL
+    for (; prefetch_k < DispatchPolicy::Stages; prefetch_k++) {
+      prefetch(tiled_prefetch_a, pAgA(_, _, _, prefetch_k));
+      prefetch(tiled_prefetch_b, pBgB(_, _, _, prefetch_k));
+    }
+
+    CUTLASS_PRAGMA_UNROLL
+    for (int k_tile = k_start_idx; k_tile < k_tile_count + k_start_idx; k_tile++, prefetch_k++) {
+      barrier_arrive(barrier_scope);
+      // Copy gmem to rmem for the first k_tile
+      copy(mainloop.tiled_copy_a, tAgA(_,_,_,k_tile), tArA);
+      copy(mainloop.tiled_copy_b, tBgB(_,_,_,k_tile), tBrB);
+
+      // TODO: register pressure
+      convert_FP8_to_FP16(tCrA, tCrA_fp16);
+      convert_FP8_to_FP16(tCrB, tCrB_fp16);
+
+      if (prefetch_k < k_tile_count) {
+        prefetch(tiled_prefetch_a, pAgA(_, _, _, prefetch_k));
+        prefetch(tiled_prefetch_b, pBgB(_, _, _, prefetch_k));
+      }
+
+      cute::gemm(tiled_mma, tCrA_fp16, tCrB_fp16, accum);
+
+      barrier_wait(barrier_scope);
+    }
+  }
+};
+
+} // namespace cutlass::gemm::collective
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
